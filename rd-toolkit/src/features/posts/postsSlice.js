c0a274452fe5acedd84ad56d78ca13def1e53d3c
herman <hermanconnor11@gmail.com>
2022-11-20 00:15:13 -0500

    Add emojis

--- rd-toolkit/src/features/posts/postsSlice.js
+++ rd-toolkit/src/features/posts/postsSlice.js
@@ -7,12 +7,26 @@ const initialState = [
     title: 'Learning Redux Toolkit',
     content: 'A little confusing.',
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: '2',
     title: 'Slices...',
     content: 'Getting the hang of this.',
     date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
@@ -32,15 +46,30 @@ const postsSlice = createSlice({
             content,
             date: new Date().toISOString(),
             userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
+    },
+    addReaction: (state, action) => {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
     },
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
 
-export const { addPost } = postsSlice.actions;
+export const { addPost, addReaction } = postsSlice.actions;
 
 export default postsSlice.reducer;
